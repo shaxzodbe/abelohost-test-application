f56d1d67ba3f5efdf1aa2c91a5f57a10e163dc73
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2025-12-27 14:00:13 +0500

    fix: Resolve CSS compilation error by refactoring SCSS base styles and updating Sass module usage.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,9 @@ composer install --no-interaction --prefer-dist
 echo "Running database migrations and sha..."
 php seed.php
 
+mkdir -p /var/www/html/storage/smarty/templates_c
+mkdir -p /var/www/html/storage/smarty/cache
+
 chown -R www-data:www-data /var/www/html/storage
 
 echo "Starting Apache..."
